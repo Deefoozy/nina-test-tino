ee98ee960ba6513bce2428413efee8bd12210359
Valentino Trok <v.trok@youweagency.com>
2023-04-11 16:59:19 +0200

    Updated querybuilding helper to check if filter key exists, differentiate between query types and added a method for each query type

--- app/Helpers/FilterHelper.php
+++ app/Helpers/FilterHelper.php
@@ -6,7 +6,32 @@ use Illuminate\Database\Query\Builder;
 
 class FilterHelper
 {
-    public static function applyFilters(Builder $query, array $filterDefinition, array $values): Builder {
+    public static function applyFilters(Builder $query, array $filterDefinitions, array $values): Builder {
+        foreach ($filterDefinitions as $filter) {
+            if (!array_key_exists($filter['field'], $values)) {
+                continue;
+            }
+
+            switch ($filter['query']) {
+                case 'where like':
+                    $query = self::addWhereLike($query, $filter, $values[$filter['field']]);
+                    break;
+                case 'where':
+                    $query = self::addWhere($query, $filter, $values[$filter['field']]);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return $query;
+    }
+
+    public static function addWhereLike(Builder $query, array $filterDefinition, array $value): Builder {
+        return $query;
+    }
+
+    public static function addWhere(Builder $query, array $filterDefinition, array $value): Builder {
         return $query;
     }
 }
