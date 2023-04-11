eb4cf846a6e10a1b2b6ae961bb018d006230528e
Valentino Trok <v.trok@youweagency.com>
2023-04-11 16:02:53 +0200

    Updated getAll endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function getAll() {
-        //
+    public function getAll(): JsonResponse
+    {
+        $users = User::all();
+
+        return response()->json(['data' => $users]);
     }
 
-    public function get(int $id) {
+    public function get(int $id): JsonResponse {
         $user = User::FindOrFail($id);
         $user->load('languages');
         $user->load('allergies');
