d583148103c5d591b187c69ed2b13f461d9edc1a
Valentino Trok <v.trok@youweagency.com>
2023-04-11 15:26:13 +0200

    Updated user model with fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'age',
+        'location',
+        'personalities',
+        'diet',
+        'religion',
+        'gender',
     ];
 
     /**
